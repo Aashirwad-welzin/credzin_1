95ae0242bd90f2b21191471a1d5a03d279e6dace
unknown <aashirwadk@welz.in>
2025-05-29 18:08:29 +0530

    changes mongo data loader

--- pycode/src/DataLoaders/MongoDB.py
+++ pycode/src/DataLoaders/MongoDB.py
@@ -1,38 +1,36 @@
 import pandas as pd
 import pymongo  
 from pymongo import MongoClient 
-import logging
-# Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger()
+from src.Utils.utils import logger
 
 
-def fetch_data_from_mongodb():
+def mongodb_client():
 
     logger.info("Connecting to MongoDB to fetch data...")
-
-    MONGO_URI = "mongodb+srv://Welzin:yYsuyoXrWcxPKmPV@welzin.1ln7rs4.mongodb.net/credzin?retryWrites=true&w=majority&appName=Welzin"
-    db_name = "credzin"
-    collection_user = "users"
-    collection_credit_card = "credit_cards"
-    collection_recommendation = "recommendations1"
-
     try:
+        MONGO_URI = "mongodb+srv://Welzin:yYsuyoXrWcxPKmPV@welzin.1ln7rs4.mongodb.net/credzin?retryWrites=true&w=majority&appName=Welzin"
+        db_name = "credzin"
         myclient = MongoClient(MONGO_URI)
         mydb = myclient[db_name]
+    
+        logger.info("Connected to MongoDB successfully.")
 
-        mycol_user = mydb[collection_user]
-        mycol_credit_card = mydb[collection_credit_card]
-        mycol_recommendation = mydb[collection_recommendation]
+        # Define collection names
+        # collection_user = "users"       
+        # collection_credit_card = "credit_cards"
+        # collection_recommendation = "recommendations
 
-        df_users = pd.DataFrame(list(mycol_user.find()))
-        df_credit_cards = pd.DataFrame(list(mycol_credit_card.find()))
-        df_recommendations = pd.DataFrame(list(mycol_recommendation.find()))
-
-        logger.info("Successfully fetched data from MongoDB.")
-        return df_users, df_credit_cards, df_recommendations
-
+        # mycol_user = mydb[collection_user]
+        # mycol_credit_card = mydb[collection_credit_card]
+        # mycol_recommendation = mydb[collection_recommendation]
+        # df_users = pd.DataFrame(list(mycol_user.find()))
+        # df_credit_cards = pd.DataFrame(list(mycol_credit_card.find()))
+        # df_recommendations = pd.DataFrame(list(mycol_recommendation.find()))
+       
     except Exception as e:
         logger.error("Error connecting to MongoDB: %s", e)
         print("Error connecting to MongoDB:", e)
-        return pd.DataFrame(), pd.DataFrame(), pd.DataFrame()  # Return empty DataFrames on error
+        return pd.DataFrame(), pd.DataFrame(), pd.DataFrame() 
+       
+
+    # Return empty DataFrames on error
